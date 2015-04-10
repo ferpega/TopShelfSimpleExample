31cd7a406a62c24b80a9d6dbe8c79151aed8a783
Fernando Pérez <fperez@percea.com>
2015-04-10 21:03:42 +0200

    Service & logging is running.

---
+++ WindowsServiceTopShelf/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WindowsServiceTopShelf
+{
+    class Program
+    {
+
+        // Command line reference:
+        //    http://topshelf.readthedocs.org/en/latest/overview/commandline.html
+        //
+        // ..\WindowsServiceTopShelf.exe help
+
+        static void Main(string[] args)
+        {
+            //HostFactory.Run(x =>            
+            var host = HostFactory.New(x =>
+                {
+                    x.Service<ServiceImplementation>(s =>
+                        {
+                            s.ConstructUsing(name => new ServiceImplementation());
+                            s.WhenStarted(si => si.Start());
+                            s.WhenStopped(si => si.Stop());                                
+                        });
+                    x.RunAsLocalSystem();
+                    x.SetDescription("AAAAA Service Description");
+                    x.SetDisplayName("AAAAA Service Display Name");
+                    x.SetServiceName("AAAAAServiceName");
+                });
+            host.Run();
+        }
+    }
+}
---
+++ WindowsServiceTopShelf/ServiceImplementation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace WindowsServiceTopShelf
+{
+    public class ServiceImplementation
+    {
+        readonly Timer _timer1;
+
+        public ServiceImplementation()
+        {
+            _timer1 = new Timer(1000) 
+                            { 
+                                AutoReset = true 
+                            };
+            _timer1.Elapsed += ShowElapsedTime;
+        }
+
+        public void Start() 
+        { 
+            _timer1.Start(); 
+        }
+        public void Stop() 
+        { 
+            _timer1.Stop(); 
+        }
+
+
+        private void ShowElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            var msg = string.Format("The service elapsed time: {0}", DateTime.Now);
+            if (Environment.UserInteractive) {
+                Console.WriteLine(msg);
+            } else {
+                System.Diagnostics.Trace.WriteLine(msg);
+            }
+        }
+
+    }
+}
